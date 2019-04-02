6eae1e28058321ce786db3be50f28636214427eb
Cara Weggler <cweggler@gmail.com>
2019-04-02 17:07:39 -0500

    Initial Commit

---
+++ Quotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quotes
+//
+//  Created by Cara on 4/2/19.
+//  Copyright © 2019 Cara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
